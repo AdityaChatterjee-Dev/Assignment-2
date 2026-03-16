cf0048be3da83ef5469458391fdabfe36e19473e
AdityaChatterjee-Dev <chatterjeeadi9@gmail.com>
2026-03-16 22:01:53 +0530

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
-import Header from "./Header";
-import StudentCard from "./StudentCard";
-import Footer from "./Footer";
-
 function App() {
+  let num1 = Number(prompt("Enter first number:"));
+  let num2 = Number(prompt("Enter second number:"));
+
+  const add = (a, b) => a + b;
+  const subtract = (a, b) => a - b;
+  const multiply = (a, b) => a * b;
+  const divide = (a, b) => b !== 0 ? a / b : "Cannot divide by zero";
+
   return (
-<div>
-<Header />
-
-<StudentCard name="Shashi" age={22} course="React" />
-<StudentCard name="Rahul" age={23} course="Java" />
-
-<Footer />
-</div>
+    <div>
+      <p>Addition: {add(num1, num2)}</p>
+      <p>Subtraction: {subtract(num1, num2)}</p>
+      <p>Multiplication: {multiply(num1, num2)}</p>
+      <p>Division: {divide(num1, num2)}</p>
+    </div>
   );
 }
 
