a7a772d00fd31385b7b6e636ef55289c761c6120
AdityaChatterjee-Dev <chatterjeeadi9@gmail.com>
2026-03-16 23:21:59 +0530

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,15 @@ function App() {
   const subtract = (a, b) => a - b;
   const multiply = (a, b) => a * b;
   const divide = (a, b) => b !== 0 ? a / b : "Cannot divide by zero";
-
-  return (
-    <div>
-      <p>Addition: {add(num1, num2)}</p>
-      <p>Subtraction: {subtract(num1, num2)}</p>
-      <p>Multiplication: {multiply(num1, num2)}</p>
-      <p>Division: {divide(num1, num2)}</p>
+    return (
+    <div style={{display: "flex",justifyContent: "center", alignItems: "center", height: "100vh",  backgroundColor: "#f0f0f0", }}>
+      <div style={{border: "2px solid #333",borderRadius: "10px",padding: "20px",backgroundColor: "white", textAlign: "center", width: "300px",}}>
+        <h2>Calculator</h2>
+        <p>Addition: {add(num1, num2)}</p>
+        <p>Subtraction: {subtract(num1, num2)}</p>
+        <p>Multiplication: {multiply(num1, num2)}</p>
+        <p>Division: {divide(num1, num2)}</p>
+      </div>
     </div>
   );
 }
